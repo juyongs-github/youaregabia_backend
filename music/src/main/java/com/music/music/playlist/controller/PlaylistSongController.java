e08eb0693841e17f4e083f855edb8f033124ecfd
KimGyeongmin <kkm01054354772@gmail.com>
2026-01-26 16:15:46 +0900

    01/26 병합 후

--- music/src/main/java/com/music/music/playlist/controller/PlaylistSongController.java
+++ music/src/main/java/com/music/music/playlist/controller/PlaylistSongController.java
@@ -3,36 +3,48 @@ package com.music.music.playlist.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.music.music.playlist.entity.PlaylistSong;
+import com.music.music.api.entity.SongDTO;
 import com.music.music.playlist.service.PlaylistSongService;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RequestMapping("/song")
+@RequestMapping("/playlists")
 @RequiredArgsConstructor
 @RestController
 public class PlaylistSongController {
 
     private final PlaylistSongService playlistSongService;
 
-    @PostMapping("/playlist/{playlistId}/song/{songId}")
-    public ResponseEntity<Void> postSong(@PathVariable Long playlistId, @PathVariable Long songId) {
+    // 곡 추가
+    @PostMapping("/{playlistId}/songs/{songId}")
+    public ResponseEntity<Void> postSongToPlaylist(@PathVariable Long playlistId, @PathVariable Long songId) {
+
         playlistSongService.addSongToPlaylist(playlistId, songId);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @DeleteMapping("playlist-song/{playlistSongId}")
-    public ResponseEntity<Void> removeSong(@PathVariable Long playlistSongId) {
+    // 곡 삭제
+    @DeleteMapping("/songs/{playlistSongId}")
+    public ResponseEntity<Void> removeSongFromPlaylist(@PathVariable Long playlistSongId) {
         playlistSongService.removeSongFromPlaylist(playlistSongId);
 
         return ResponseEntity.noContent().build();
     }
+
+    // 곡 조회
+    @GetMapping("/{playlistId}/songs")
+    public ResponseEntity<List<SongDTO>> getSongsByPlaylist(@PathVariable Long playlistId) {
+        return ResponseEntity.ok(playlistSongService.getSongsByPlaylist(playlistId));
+    }
+
 }

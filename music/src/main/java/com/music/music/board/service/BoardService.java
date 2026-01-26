2d80ed6b9afd48a36a7184e336f1f74ffeb27ae3
yunkiseon <yks97016033@gmail.com>
2026-01-26 09:22:52 +0900

    페이징 중

--- music/src/main/java/com/music/music/board/service/BoardService.java
+++ music/src/main/java/com/music/music/board/service/BoardService.java
@@ -2,6 +2,10 @@ package com.music.music.board.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +17,7 @@ import com.music.music.board.repository.BoardRepository;
 import com.music.music.board.repository.ReplyLikeRepository;
 import com.music.music.board.repository.ReplyRepository;
 import com.music.music.board.repository.UserRepository;
+import com.music.music.common.PageRequestDTO;
 import com.music.music.user.User;
 
 import lombok.AllArgsConstructor;
@@ -28,10 +33,13 @@ public class BoardService {
     private final ReplyService replyService;
 
 
-    public List<BoardDto> getBoardList() {
-    List<Board> boards = boardRepository.findAll();
+    public List<BoardDto> getBoardList(PageRequestDTO dto) {
+        Page<Board> result = null;
+        Pageable pageable = PageRequest.of(dto.getPage(), dto.getSize(), Sort.by("id").descending());
+        result = boardRepository.findAll(pageable);
+
     
-    return boards.stream()
+    List<BoardDto> dtoList = result.stream()
             .map(BoardDto::new)  // 댓글 없는 생성자 사용
             .toList();
 }

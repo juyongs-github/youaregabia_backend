95e4311ad8920b85b408a0a59160eae3038bfe61
KimGyeongmin <kkm01054354772@gmail.com>
2026-01-29 18:20:37 +0900

    01/29 컨트롤러 수정

--- music/src/main/java/com/music/music/playlist/controller/PlaylistController.java
+++ music/src/main/java/com/music/music/playlist/controller/PlaylistController.java
@@ -16,9 +16,12 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequestMapping("/playlist")
 @RequiredArgsConstructor
@@ -27,11 +30,13 @@ public class PlaylistController {
     private final PlaylistService playlistService;
     private final UserRepository userRepository;
 
+    // 플레이리스트 전체 조회 (/playlist/all + GET)
     @GetMapping("/all")
     public List<PlaylistDTO> getAllPlaylists() {
         return playlistService.getAllPlaylists();
     }
 
+    // 플레이리스트 생성 (/playlist + POST)
     @PostMapping
     public ResponseEntity<Void> createPlaylist(
             @RequestParam MultipartFile file,
@@ -42,4 +47,27 @@ public class PlaylistController {
         return ResponseEntity.ok().build();
     }
 
+    // 수정한 부분
+
+    // 플레이리스트 상세 조회 (/playlist/{id} + GET)
+    @GetMapping("/{id}")
+    public PlaylistDTO getPlaylist(@PathVariable Long id) {
+
+        return playlistService.getPlaylist(id);
+    }
+
+    // 플레이리스트 수정 (/playlist/{id} + PUT)
+    @PutMapping("/{id}")
+    public PlaylistDTO putPlaylist(@PathVariable Long id, @RequestBody PlaylistDTO dto) {
+
+        return playlistService.updatePlaylist(id, dto);
+    }
+
+    // 플레이리스트 삭제 (/playlist/{id} + DELETE)
+    @DeleteMapping("/{id}")
+    public String deletePlaylist(@PathVariable Long id) {
+        playlistService.deletePlaylist(id);
+
+        return "삭제완료";
+    }
 }

eabfaf5a9413841ea0c0d55ae5b1913bf6b447cb
yunkiseon <yks97016033@gmail.com>
2026-04-01 18:17:00 +0900

    랭킹 수정, 아직 boardSong 미완

--- music/src/main/java/com/music/music/board/dto/BoardSongDto.java
+++ music/src/main/java/com/music/music/board/dto/BoardSongDto.java
@@ -13,12 +13,20 @@ public class BoardSongDto {
     private String artistName;
     private String imgUrl;
     private int orderIndex;
+    private String previewUrl;   // 추가
+    private String genreName;    // 추가
+    private Long durationMs;     // 추가
+    private String releaseDate;  // 추가
 
     public BoardSongDto(BoardSong boardSong) {
         this.songId = boardSong.getSong().getId();
         this.trackName = boardSong.getSong().getTrackName();
         this.artistName = boardSong.getSong().getArtistName();
         this.imgUrl = boardSong.getSong().getImgUrl();
+        this.previewUrl = boardSong.getSong().getPreviewUrl(); 
+        this.genreName = boardSong.getSong().getGenreName();
+        this.durationMs = boardSong.getSong().getDurationMs();
+        this.releaseDate = boardSong.getSong().getReleaseDate();
         this.orderIndex = boardSong.getOrderIndex();
     }
 }
